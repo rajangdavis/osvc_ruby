d1185bea9e5604a119584a79247cdb30e2d11501
rajangdavis <rajangdavis@gmail.com>
2017-01-17 10:22:37 -0800

    hopefully this is better but I merged the shared functionality between the service_product and service_category into a class that they both inherit

--- lib/osc_ruby.rb
+++ lib/osc_ruby.rb
@@ -3,6 +3,7 @@ module OSCRuby; end
 require 'osc_ruby/client'
 require 'osc_ruby/connect'
 require 'osc_ruby/service_class'
+require 'osc_ruby/product_category_shared'
 require 'osc_ruby/service_product'
 require 'osc_ruby/service_category'
 require 'osc_ruby/answer'
