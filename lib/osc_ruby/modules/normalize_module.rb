b74e098032bfda98a7ffcb6d6d03e2e0b847cdc7
Rajan Davis <rajangdavis@gmail.com>
2017-08-19 00:00:55 -0700

    Hopefully this code is better and makes the code climate better

--- lib/osc_ruby/modules/normalize_module.rb
+++ lib/osc_ruby/modules/normalize_module.rb
@@ -16,24 +16,16 @@ module OSCRuby
 
 					json_input = JSON.parse(input.body)
 
-					# initialize an array to hold all of the objects
 					final_hash = []
 
-					# loop through the items from the returned JSON response
 					json_input['items'].each do |item|
-
-							results_array = iterate_through_rows(item)							
-
-			    			# puts "this is an array:  #{final_hash}"
-					        final_hash.push(results_array)
-
-
+						results_array = iterate_through_rows(item)							
+				        final_hash.push(results_array)
 					end
 
 					if final_hash.size === 1
 						final_hash = final_hash.flatten!
 					end
-
 
 					final_hash.to_json
 
@@ -45,23 +37,17 @@ module OSCRuby
 
 				results_array = []
 
-		        # loop through rows
 		        item['rows'].each_with_index do |row,_row_i|
 
-		                # initialize a hash to create the object
-		                obj_hash = {}
-		                
-		                # loop through the column names from the query
-		                item['columnNames'].each_with_index do |column,i|
+	                obj_hash = {}
+	                
+	                item['columnNames'].each_with_index do |column,i|
 
-		                        # set the object property to the value of the row
-		                        # where the index of the value within that row
-		                        # matches the index of the column name
-		                        obj_hash[column] = if !row[i].nil? && row[i].is_i? == true then row[i].to_i else row[i] end
-		                end
+                        obj_hash[column] = if !row[i].nil? && row[i].is_i? == true then row[i].to_i else row[i] end
 
-		    			# push the hash into the array
-		    			results_array.push(obj_hash)
+	                end
+
+	    			results_array.push(obj_hash)
 
 		        end
 
--- lib/osc_ruby/version.rb
+++ lib/osc_ruby/version.rb
@@ -1,3 +1,3 @@
 module OSCRuby
-  VERSION = "1.2.1"
+  VERSION = "1.2.2"
 end
